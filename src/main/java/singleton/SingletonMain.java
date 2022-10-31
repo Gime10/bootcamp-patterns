0c04670e61a4c4a6951f644a955b3de535f8d0ca
Gimena Mariaca <xgimena@gmail.com>
2022-10-30 20:37:34 -0400

    Added MySQL client singleton

--- src/main/java/singleton/SingletonMain.java
+++ src/main/java/singleton/SingletonMain.java
@@ -1,5 +1,7 @@
 package singleton;
 
+import java.sql.SQLException;
+
 public class SingletonMain {
 
     public static void main(String[]arg){
@@ -10,6 +12,17 @@ public class SingletonMain {
         MongoDBClient mongoDBClient3 = MongoDBClient.getClient();
         System.out.println(mongoDBClient3.getData());
 
-
+        try {
+            MySQLClient mySQLClient1 = MySQLClient.getInstance();
+            System.out.println(mySQLClient1.getAllUsers().toString());
+            MySQLClient mySQLClient2 = MySQLClient.getInstance();
+            System.out.println(mySQLClient2.getAllUsers().toString());
+            MySQLClient mySQLClient3 = MySQLClient.getInstance();
+            System.out.println(mySQLClient3.getAllUsers().toString());
+        } catch (SQLException sqlException) {
+            System.out.println("Error Message: " + sqlException.getMessage());
+            System.out.println("SQLState: " + sqlException.getSQLState());
+            System.out.println("Error Code: " + sqlException.getErrorCode());
+        }
     }
 }
